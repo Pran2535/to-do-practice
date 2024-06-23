84aed4aa34a3f3e7c9d22493fba0c7fe5242092f
Pran2535 <126868179+Pran2535@users.noreply.github.com>
2024-06-23 08:27:58 +0530

    first commit

---
+++ .history/src/App_20240623082148.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import TodoWrapper from "./components/TodoWrapper";
+
+const App = () => {
+  return <div className="App">
+ <TodoWrapper/>
+
+  </div>;
+};
+
+export default App;
---
+++ .history/src/components/TodoWrapper_20240623082133.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TodoWrapper = () => {
+  return (
+    <div>
+      hii
+    </div>
+  )
+}
+
+export default TodoWrapper
